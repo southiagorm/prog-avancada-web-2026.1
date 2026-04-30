101668831bf156e403c62e39798bb1f5ad7bc2eb
Thiago Rodrigues <thiago.computer@gmail.com>
2026-04-30 09:47:28 -0300

    aula 29-04, implementação das operação get(findById), put(update), delete(deletar), patch(atualização parcial)

--- src/main/java/com/aula/apibiblioteca/controller/UsuarioController.java
+++ src/main/java/com/aula/apibiblioteca/controller/UsuarioController.java
@@ -2,11 +2,13 @@ package com.aula.apibiblioteca.controller;
 
 import com.aula.apibiblioteca.model.Usuario;
 import com.aula.apibiblioteca.service.UsuarioService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 //http://localhost:8080/usuarios
@@ -21,9 +23,51 @@ public class UsuarioController {
         return ResponseEntity.ok(usuarioService.findAll());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> findById(@PathVariable Long id){
+        try{
+            Usuario usuario = usuarioService.findById(id);
+            return ResponseEntity.ok(usuario);
+        }catch(EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
     @PostMapping
     public ResponseEntity<Usuario> save(@RequestBody Usuario usuario){
         var userCreated = usuarioService.save(usuario);
         return ResponseEntity.status(201).body(userCreated);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> update(@PathVariable Long id, @RequestBody Usuario usuario){
+        try{
+            Usuario usuarioTemp = usuarioService.update(id, usuario);
+            return ResponseEntity.ok(usuarioTemp);
+        }catch(EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        try{
+            usuarioService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }catch(EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PatchMapping("/{id}/update-email")
+    public ResponseEntity<Usuario> updateEmail(@PathVariable Long id, @RequestBody Map<String, String> map){
+        try{
+            System.out.println(map.get("email"));
+            Usuario usuarioTemp = usuarioService.updateEmail(id, map.get("email"));
+            return ResponseEntity.ok(usuarioTemp);
+        }catch(EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
--- src/main/java/com/aula/apibiblioteca/service/UsuarioService.java
+++ src/main/java/com/aula/apibiblioteca/service/UsuarioService.java
@@ -2,6 +2,7 @@ package com.aula.apibiblioteca.service;
 
 import com.aula.apibiblioteca.model.Usuario;
 import com.aula.apibiblioteca.repository.UsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +14,39 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    //Select sem Where
     public List<Usuario> findAll(){
         return usuarioRepository.findAll();
     }
 
+    //SELECT * FROM WHERE id=?
+    public Usuario findById(Long id){
+        return usuarioRepository.findById(id).orElseThrow(()->new EntityNotFoundException());
+    }
+
+    //Insert
     public Usuario save(Usuario usuario){
         return usuarioRepository.save(usuario);
+    }
+
+    //Update *
+    public Usuario update(Long id, Usuario usuario){
+        Usuario usuarioTemp = usuarioRepository.findById(id).orElseThrow(()->new EntityNotFoundException());
+        usuarioTemp.setNome(usuario.getNome());
+        usuarioTemp.setEmail(usuario.getEmail());
+        return usuarioRepository.save(usuarioTemp);
+    }
+
+    //Delete com where
+    public void deleteById(Long id){
+        findById(id);
+        usuarioRepository.deleteById(id);
+    }
+
+    //Update do email
+    public Usuario updateEmail(Long id, String email){
+        Usuario usuarioTemp = usuarioRepository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+        usuarioTemp.setEmail(email);
+        return usuarioRepository.save(usuarioTemp);
     }
 }
